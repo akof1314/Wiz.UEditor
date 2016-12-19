e634c88b728ebfd43966a557651381905b647c43
WuHuan <kof1234@sina.com>
2016-12-19 20:03:45 +0800

    重命名插件防止与官方版本冲突

--- wiz_ueditor.js
+++ wiz_ueditor.js
@@ -3,7 +3,7 @@ var objWindow = objApp.Window;
 
 if (objWindow.CurrentDocument != null)
 {
-    var pluginPath = objApp.GetPluginPathByScriptFileName("baidu_editor.js");
+    var pluginPath = objApp.GetPluginPathByScriptFileName("wiz_ueditor.js");
     var editorFileName = pluginPath + "index.html?guid=" + objWindow.CurrentDocument.GUID + "&kbguid=" + objWindow.CurrentDocument.Database.KbGUID;
     //
     objWindow.ViewHtml(editorFileName, true);
